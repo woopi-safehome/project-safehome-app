bf6ba02fa3a2a6108ca19d191844b806209db351
woopi1030 <lozenkrantz@gmail.com>
2026-04-30 00:26:50 +0900

    [CHG] 개발환경설정 추가

---
+++ app.config.js
@@ -0,0 +1,59 @@
+const IS_PREVIEW = process.env.APP_ENV === 'preview';
+
+export default {
+  expo: {
+    name: IS_PREVIEW ? 'SafeHome (Dev)' : 'SafeHome',
+    slug: 'project-safehome-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'projectsafehomeapp',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      usesCleartextTraffic: IS_PREVIEW,
+      adaptiveIcon: {
+        backgroundColor: '#E6F4FE',
+        foregroundImage: './assets/images/android-icon-foreground.png',
+        backgroundImage: './assets/images/android-icon-background.png',
+        monochromeImage: './assets/images/android-icon-monochrome.png',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: 'com.anonymous.projectsafehomeapp',
+    },
+    web: {
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+          dark: {
+            backgroundColor: '#000000',
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: '99558d7f-8474-4dbc-a591-51ccfae46ff2',
+      },
+    },
+    owner: 'woopii',
+  },
+};
